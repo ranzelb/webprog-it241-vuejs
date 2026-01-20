ef54c143a8fef001a80ea633b495ed711d604df8
Ranzel Binggoy <rpbinggoy@student.apc.edu.ph>
2026-01-20 08:13:40 +0800

    color

--- firstsfc/src/components/FoodItem2.vue
+++ firstsfc/src/components/FoodItem2.vue
@@ -22,6 +22,6 @@ export default {
     display: inline-block;
     margin: 10px;
     padding: 10px;
-    background-color:  #966919, RGB (150, 105, 25);
+    background-color: orange;
   }
 </style>
