84f466ad25a42e554d1417cba2ac0e8b5f36823e
Ranzel Binggoy <rpbinggoy@student.apc.edu.ph>
2026-01-20 09:31:20 +0800

    changea

--- firstsfc/src/components/PersonalProfile.vue
+++ firstsfc/src/components/PersonalProfile.vue
@@ -1,4 +1,5 @@
-<!DOCTYPE html>
+<template>
+ <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -81,119 +82,8 @@
 </body>
 </html>
 
-body {
-    background-image: url('https://scontent.fmnl17-3.fna.fbcdn.net/v/t1.15752-9/610459653_1632231908215702_2996603428213222403_n.jpg?stp=dst-jpg_s640x640_tt6&_nc_cat=106&ccb=1-7&_nc_sid=0024fc&_nc_ohc=Si5tpmVFuVQQ7kNvwF5VvV6&_nc_oc=AdlG0rLHKI6lRIhTXXDs-tCzLfPwvwKgCx9PhA3gmSW5uIFxIMtidH_JY8czGc1V2Mo&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fmnl17-3.fna&oh=03_Q7cD4QGfXga0iQ_252fCeKWOwhtrnEmUTCde5VuvrUSAiAdo3A&oe=69833EBC');
-    background-size: cover;
-    background-position: center;
-    background-attachment: fixed;
-    background-repeat: no-repeat;
-    font-family: Arial, sans-serif;
-    text-align: center;
-    margin: 0;
-    padding: 0;
-}
+</template>
 
+<script></script>
 
-h1 {
-    color: white;
-    background-color: rgba(0, 51, 102, 0.85);
-    display: inline-block;
-    padding: 20px 40px;
-    margin: 30px auto;
-    border-radius: 15px;
-    text-shadow: 2px 2px 8px rgba(0, 0, 0, 0.7);
-    box-shadow: 0 4px 15px rgba(0, 0, 0, 0.5);
-    font-size: 36px;
-    letter-spacing: 2px;
-}
-
-
-p {
-    color: black;
-    text-shadow: 1px 1px 5px gray;
-    font-size: 18px;
-}
-
-
-img {
-    margin-top: 20px;
-    border: 3px solid lightblue;
-    box-shadow: 0 0 20px lightblue;
-}
-
-
-.container {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    margin-top: 30px;
-}
-
-
-.container div {
-    background-color: white;
-    border: 1px solid lightblue;
-    width: 300px;
-    margin: 10px;
-    color: black;
-    font-size: 18px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    box-shadow: 0 0 15px white;
-    padding: 15px;
-    box-sizing: border-box;
-}
-
-
-.container div:hover {
-    background-color: lightblue;
-    color: black;
-    cursor: pointer;
-}
-
-
-.container ul {
-    list-style-type: disc;
-    padding-left: 20px;
-    text-align: left;
-}
-
-.container li {
-    font-weight: bold;
-    color: black;
-    margin-bottom: 8px;
-}
-
-
-.container a {
-    color: #003366;
-    text-decoration: none;
-    word-break: break-all;
-}
-
-.container a:hover {
-    text-decoration: underline;
-    color: #0055aa;
-}
-
-
-.container-bottom {
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    background-color: white;
-    padding: 10px 0;
-    text-align: center;
-    box-shadow: 0 -5px 10px rgba(255, 0, 0, 0.5);
-    z-index: 1000;
-}
-
-.container-bottom .container div {
-    width: 200px;
-    font-size: 16px;
-    padding: 10px;
-
-}
-
+<style></style>
