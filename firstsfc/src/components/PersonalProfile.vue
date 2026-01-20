aad3bac57b61479d1b31f041ef615871aac2a3a8
Ranzel Binggoy <rpbinggoy@student.apc.edu.ph>
2026-01-20 09:19:26 +0800

    task 03

---
+++ firstsfc/src/components/PersonalProfile.vue
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/flexbox.css">
+
+    <title>WEBPROG IT241</title>
+   
+</head>
+<body>
+
+<h1>My Personal Profile Web page</h1>
+<p>
+<img src="https://scontent.fmnl17-2.fna.fbcdn.net/v/t1.15752-9/607059133_4386102214944020_8834524784734252105_n.png?stp=dst-png_p480x480&_nc_cat=107&ccb=1-7&_nc_sid=0024fc&_nc_ohc=4U0ROIfUSKQQ7kNvwHc8L8P&_nc_oc=Adk8iGlEEZWznVZNe9Osxh7ImxQLW8iBe7-aP9iCpFExR_lLIESpp0GcEU8K7tVLfjk&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fmnl17-2.fna&oh=03_Q7cD4QEuDOAtzD3Ui3hE8oQg_wJy5rRF8SxrkyXtQBRC-mStfQ&oe=698303D2" alt="Picture of me" width="500" height="600">
+</p>
+
+<div class="container">
+
+  <div>
+    <p><strong>About Me</strong></p>
+    <ul>
+      <li><strong>Name:</strong> Ranzel John P. Binggoy</li>
+      <li><strong>Age:</strong> 19</li>
+      <li><strong>Personality:</strong> Friendly</li>
+      <li><strong>Course:</strong> Bachelor of Science in Information Technology</li>
+    </ul>
+  </div>
+
+  <div>
+    <p><strong>School / Education</strong></p>
+    <ul>
+      <li>Bangkal Elementary School</li>
+      <li>Bangkal High School Main</li>
+      <li>Maximo Estrella Senior High School</li>
+      <li>Asia Pacific College</li>
+    </ul>
+  </div>
+
+  <div>
+    <p><strong>Hobbies</strong></p>
+    <ul>
+      <li>Gaming</li>
+      <li>Anime</li>
+      <li>Sports</li>
+      <li>Travel</li>
+    </ul>
+  </div>
+
+  <div>
+    <p><strong>Skills</strong></p>
+    <ul>
+      <li>Oral Communication</li>
+      <li>Critical Thinking</li>
+      <li>Problem Solving</li>
+      <li>Computer Literacy</li>
+      <li>Leadership</li>
+    </ul>
+  </div>
+
+  <div>
+    <p><strong>Goal</strong></p>
+    <ul>
+      <li>Graduate from college</li>
+      <li>Become financially stable</li>
+      <li>Start a startup company</li>
+    </ul>
+  </div>
+
+  <div>
+    <p><strong>Resources</strong></p>
+    <ul>
+      <li><a href="https://www.codechef.com/html-online-compiler" target="_blank">HTML Online Compiler</a></li>
+      <li><a href="https://chatgpt.com/share/695ba739-0cc8-800f-ba66-4d50d99f9a4e" target="_blank">ChatGPT Reference</a></li>
+      <li><a href="https://scontent.fmnl17-2.fna.fbcdn.net/v/t1.15752-9/607059133_4386102214944020_8834524784734252105_n.png?stp=dst-png_p480x480&_nc_cat=107&ccb=1-7&_nc_sid=0024fc&_nc_ohc=4U0ROIfUSKQQ7kNvwHc8L8P&_nc_oc=Adk8iGlEEZWznVZNe9Osxh7ImxQLW8iBe7-aP9iCpFExR_lLIESpp0GcEU8K7tVLfjk&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fmnl17-2.fna&oh=03_Q7cD4QEuDOAtzD3Ui3hE8oQg_wJy5rRF8SxrkyXtQBRC-mStfQ&oe=698303D2" target="_blank">Profile Image Source</a></li>
+    </ul>
+  </div>
+    
+</div>
+
+</body>
+</html>
--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 
 import App from './App.vue'
+import PersonalProfile from './components/PersonalProfile.vue'
 import FoodItem from './components/FoodItem.vue'
 import FoodItem2 from './components/FoodItem2.vue'
 
 const app = createApp(App)
+app.component('personal-profile', PersonalProfile)
 app.component('food-item', FoodItem)
 app.component('food-item2', FoodItem2)
 app.mount('#app')
